dd0e1ae2a3b948696579b391450b852b45423b35
kunal-arora-1411 <kunalar2003@gmail.com>
2026-03-23 00:45:27 +0530

    deploy2

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/portfolio3/",
+  base: process.env.GITHUB_PAGES ? "/portfolio3/" : "/",
   plugins: [react()],
   build: {
     rollupOptions: {
